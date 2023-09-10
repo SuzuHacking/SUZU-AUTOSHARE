c297406e2392689807e8f7ac9730762968e144d5
SuzuHacking <144612022+SuzuHacking@users.noreply.github.com>
2023-09-11 03:10:12 +0800

    Create SUZUSHARE.py

---
+++ SUZUSHARE.py
@@ -0,0 +1,14 @@
+import os, platform, time, sys
+ 
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+ 
+
+os.system('xdg-open https://www.facebook.com/100080616663558')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ import SHARE1
+elif bit == '32bit':
+ import SHARE2_32
